7d3c1d97af3368b01a7f4f17716a843866c50ad1
Mathewer987 <111314558+Mathewer987@users.noreply.github.com>
2024-06-08 18:16:15 -0300

    ModificacionesFinales

--- Assets/Scripts/RandomPlacement.cs
+++ Assets/Scripts/RandomPlacement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RandomPlacement : MonoBehaviour
 {
     public float offset;
+    
     public void SetRandomPosition()
     {
         Vector3 newPosition = new Vector3(Random.Range(0,offset), 9, Random.Range(0, offset));
